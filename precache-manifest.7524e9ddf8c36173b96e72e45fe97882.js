adc40a3ac88bc0a5bf83c23a4616e5273f78b522
patel-jeni <jenipatel006@gmail.com>
2020-07-26 01:51:55 -0700

    Updates

---
+++ precache-manifest.7524e9ddf8c36173b96e72e45fe97882.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "665729f27315296c6a48146f238650e0",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "2df39200fad7ff75b4eb",
+    "url": "/my-react-app/static/css/main.36c4737a.chunk.css"
+  },
+  {
+    "revision": "3788dd95357404788ad5",
+    "url": "/my-react-app/static/js/2.5bf06b29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-react-app/static/js/2.5bf06b29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2df39200fad7ff75b4eb",
+    "url": "/my-react-app/static/js/main.b3cf14f3.chunk.js"
+  },
+  {
+    "revision": "435064591b6543e8ca17",
+    "url": "/my-react-app/static/js/runtime-main.c0584337.js"
+  }
+]);
